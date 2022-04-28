ecd59e47ade81a5accc522cd35b953d5aed7eb6f
Rheygine Medel <quic_rmedel@quicinc.com>
2022-04-28 13:47:44 -0700

    sf: Avoid destroying a null layer
    Change-Id: I94176f521ed25d49e7a01b713a7d87ae3751c707
    CRs-Fixed: 3183566
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -5450,6 +5450,11 @@ void SurfaceFlinger::onHandleDestroyed(BBinder* handle, sp<Layer>& layer, uint32
         mDestroyedHandles.emplace_back(layerId);
     }
 
+    if (!layer) {
+      ALOGW("Attempted to destroy an invalid layer");
+      return;
+    }
+
     Mutex::Autolock lock(mStateLock);
     markLayerPendingRemovalLocked(layer);
     layer->onHandleDestroyed();
