2aac45dc374f38bbb12b2f96fda2a3b13be60284
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-09 20:28:56 +0530

    SF: Bring back support for disabling backpressure propagation
    Taken from CLO (QSSI 13). Some Qualcomm devices can still
    benefit from disabling backpressure propagation by setting:
    
    debug.sf.disable_backpressure=1
    
    Change-Id: I2346a1b314666706e1299b295f13b2cef6e00b4d

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -1253,6 +1253,7 @@ private:
     std::atomic_bool mForceFullDamage = false;
 
     bool mLayerCachingEnabled = false;
+    bool mPropagateBackpressure = true;
     bool mBackpressureGpuComposition = false;
 
     LayerTracing mLayerTracing;
