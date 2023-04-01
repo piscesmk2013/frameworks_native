AOSPA/android_frameworks_native

sf: Check if fps scores are valid to compute best refresh rate.
If a layer requests an fps that is outside display range, all possible
frame rates end up in score zero. Trying to find a best among those would result in picking max or min fps which isn't appropriate.

Instead, avoid change in fps by picking current fps.

CRs-Fixed: 3276129
Change-Id: Icf643bff5accfaefa492a5b5ba5627188856faf1

services/surfaceflinger/Scheduler/RefreshRateConfigs.cpp
@@ -467,7 +467,14 @@ auto RefreshRateConfigs::getBestRefreshRateLocked(const std::vector<LayerRequire
             ? getMaxScoreRefreshRate(scores.rbegin(), scores.rend())
             : getMaxScoreRefreshRate(scores.begin(), scores.end());
 
-    if (primaryRangeIsSingleRate) {
+   bool noFpsScored = std::all_of(scores.begin(), scores.end(),
+                                  [](RefreshRateScore score) { return score.score == 0; });
+   if (noFpsScored) {
+      ALOGV("No fps scored - choose %s", to_string(mActiveModeIt->second->getFps()).c_str());
+      return {mActiveModeIt->second, kNoSignals};
+   }
+
+   if (primaryRangeIsSingleRate) {
         // If we never scored any layers, then choose the rate from the primary
         // range instead of picking a random score from the app range.
         if (std::all_of(scores.begin(), scores.end(),

piscesmk2013/frameworks_native

services/surfaceflinger/Scheduler/RefreshRateConfigs.cpp
@@ -553,7 +553,14 @@ auto RefreshRateConfigs::getBestRefreshRateLocked(const std::vector<LayerRequire
         return {bestRefreshRate, kNoSignals};
     };
 
-    if (primaryRangeIsSingleRate) {
+   bool noFpsScored = std::all_of(scores.begin(), scores.end(),
+                                  [](RefreshRateScore score) { return score.overallScore == 0; });
+   if (noFpsScored) {
+      ALOGV("No fps scored - choose %s", to_string(mActiveModeIt->second->getFps()).c_str());
+      return {mActiveModeIt->second, kNoSignals};
+   }
+
+   if (primaryRangeIsSingleRate) {
         // If we never scored any layers, then choose the rate from the primary
         // range instead of picking a random score from the app range.
         if (std::all_of(scores.begin(), scores.end(),