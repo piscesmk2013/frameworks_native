b2529a0a5c1357797cfe068a20c2e951501ab569
Hao Chen <chenhaosjtuacm@google.com>
2023-05-31 14:12:33 -0700

    Add Default Constructor Required by libstdc++
    Test: Build with CMake. See aosp/2629369.
    Bug: 285204695
    Change-Id: I105552df72fe8b4f15f75192a5c36480c1bdc5c7

--- libs/binder/include/binder/Parcel.h
+++ libs/binder/include/binder/Parcel.h
@@ -1280,6 +1280,7 @@ private:
 
     // Fields only needed when parcelling for "kernel Binder".
     struct KernelFields {
+        KernelFields() {}
         binder_size_t* mObjects = nullptr;
         size_t mObjectsSize = 0;
         size_t mObjectsCapacity = 0;
